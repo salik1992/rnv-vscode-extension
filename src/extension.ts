9aa619239f2775a7ba6828e29925b13105a7ae83
Vit Salomon <vit.salomon@24i.com>
2021-01-29 14:50:09 +0100

    feat: run rnv from command pallete

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,13 @@
 import * as vscode from 'vscode';
 import { RNVTasksTreeView } from './rnv/view';
-import { launch, copy } from './rnv/actions';
+import { launch, copy, start, build, run, deploy } from './rnv/actions';
 
 export function activate() {
     vscode.commands.registerCommand('extension.rnv.launch', launch);
     vscode.commands.registerCommand('extension.rnv.copy', copy);
+    vscode.commands.registerCommand('extension.rnv.start', start);
+    vscode.commands.registerCommand('extension.rnv.run', run);
+    vscode.commands.registerCommand('extension.rnv.build', build);
+    vscode.commands.registerCommand('extension.rnv.deploy', deploy);
     vscode.window.registerTreeDataProvider('rnv', new RNVTasksTreeView());
 }
--- src/rnv/actions.ts
+++ src/rnv/actions.ts
@@ -1,6 +1,6 @@
 import * as vscode from 'vscode';
 import { getTasks } from './getTasks';
-import { Task, TaskByPlatform } from './types';
+import { Task } from './types';
 import { RNVTreeItem } from './view';
 
 export const taskToCommand = (task: Task) => {
@@ -30,3 +30,34 @@ export const copy = (item: RNVTreeItem) => {
     // Only final item tasks can run this command
     vscode.env.clipboard.writeText(taskToCommand(item.data as unknown as Task));
 };
+
+const ask = async (picks: string[], placeHolder: string) => (
+    (await vscode.window.showQuickPick(
+        picks.map((label) => ({ label })),
+        { canPickMany: false, placeHolder }
+    ) ?? { label: null }).label
+);
+
+const askForTask = async (action: string): Promise<Task | null> => {
+    const platforms = await getTasks();
+    const platform = await ask(Object.keys(platforms), 'PLATFORM');
+    if (platform === null) return null;
+    const appConfigs = platforms[platform];
+    const appConfig = await ask(Object.keys(appConfigs), 'APP CONFIG');
+    if (appConfig === null) return null;
+    const buildSchemes = appConfigs[appConfig];
+    const buildScheme = await ask(Object.keys(buildSchemes), 'BUILD SCHEME');
+    if (buildScheme === null) return null;
+    return { action, platform, appConfig, buildScheme, isTask: true };
+};
+
+const askAndLaunch = async (action: string) => {
+    const task = await askForTask(action);
+    if (task === null) return;
+    launch(task);
+};
+
+export const start = () => askAndLaunch('start');
+export const run = () => askAndLaunch('run');
+export const build = () => askAndLaunch('build');
+export const deploy = () => askAndLaunch('deploy');
