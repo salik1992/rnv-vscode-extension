f779ca55db3b5668159ef60d970e965367531b0e
Vit Salomon <vit.salomon@24i.com>
2021-02-15 11:57:30 +0100

    fix: commands do not show for not rnv enabled workspace

--- src/extension.ts
+++ src/extension.ts
@@ -4,6 +4,7 @@ import { launch } from './rnv/runner';
 import { copy, start, build, run, deploy, stop, favourite } from './rnv/actions';
 
 export function activate() {
+    vscode.commands.executeCommand('setContext', 'rnv:isActive', true);
     vscode.commands.registerCommand('extension.rnv.launch', launch);
     vscode.commands.registerCommand('extension.rnv.copy', copy);
     vscode.commands.registerCommand('extension.rnv.start', start);
